0169569ddb73525141874b8b4dfe7f5ff2a5693a
MACorman <madelinecorman@gmail.com>
2020-07-30 17:19:37 -0400

    got a rough outline of site. All content is on the page. Youtube embed of demos and a carousel for medium posts.

---
+++ src/BlogPostCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+// import { Carousel } from 'react-responsive-carousel';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+const tagToText = (node) => {
+  let tag = document.createElement('div')
+  tag.innerHTML = node
+  node = tag.innerText
+  return node
+}
+
+const shortenText = (text) => {
+    for(let i=0; i<text.length; i++) {
+        if(text.slice(i, i+19) === 'Continue reading on') {
+            return i
+        }
+    }
+}
+
+function BlogPostCard(props) {
+    return(
+        <Slide>
+        <div>
+            {props.thumbnail && <a href={props.link} target="_blank"><img src={props.thumbnail} alt={props.title} style={{height: 400, width: 'auto'}}/></a>}
+            <div>
+                <h5>{props.title}</h5>
+                <p>{tagToText(props.content).slice(0, shortenText(tagToText(props.content)))}</p>
+            </div>
+        </div>
+
+        </Slide>
+    )
+}
+
+export default BlogPostCard
---
+++ src/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <>
+            <h2>Contact</h2>
+            <p>madelinecorman@gmail.com</p>
+            <a href="https://www.linkedin.com/in/madeline-corman/" target="_blank">LinkedIn</a>
+            <br/>
+            <a href="https://twitter.com/MadelineCorman" target="_blank">Twitter</a>
+            <br/>
+            <a href="https://medium.com/@madelinecorman" target="_blank">Medium</a>
+            <br/>
+            <a href="https://github.com/MACorman" target="_blank">GitHub</a>
+
+        </>
+    )
+}
+
+export default Contact
