0318c7ab5d15664b3412b80faef1c2a92af94804
MACorman <madelinecorman@gmail.com>
2020-08-06 17:01:52 -0400

    projects now circulate on a carousel

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import YouTube from 'react-youtube'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 class ProjectCard extends React.Component {
 
     render() {
         return (
-            <>
+            <Slide>
                 {this.props.url ? <a href={this.props.url} target="_blank">{this.props.name}</a> : <h3>{this.props.name}</h3>}
                 <p>{this.props.description}</p>
                 <a href={this.props.frontend} target="_blank">frontend</a>
                 <br/>
                 <a href={this.props.backend} target="_blank">backend</a>
                 {this.props.demo && <YouTube videoId={this.props.demo} opts={{height: '250', width: '450'}}/>}
-            </>
+            </Slide>
         )
     }
 }
--- src/ProjectsContainer.js
+++ src/ProjectsContainer.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import projects from './ProjectData'
 import ProjectCard from './ProjectCard'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 class ProjectsContainer extends React.Component {
     
@@ -8,7 +12,21 @@ class ProjectsContainer extends React.Component {
         return (
             <>
                 <h2>Projects</h2>
-                {projects.map(p => <ProjectCard {...p} />)}
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={125}
+                    totalSlides={4}
+                    isIntrinsicHeight={true}
+                    infinite={true}
+                >
+                    <Slider>
+                        {projects.map(p => <ProjectCard {...p} />)}
+                    </Slider>
+                    <div style={{marginLeft: 'auto', marginRight: 'auto', width: 'fit-content'}}>
+                        <ButtonBack style={{border: 'none', color: 'black', backgroundColor: 'white', marginRight: '15px'}}><FontAwesomeIcon icon={faAngleLeft} color={'grey'} size={'2x'}/></ButtonBack>
+                        <ButtonNext style={{border: 'none', backgroundColor: 'white', marginLeft: '15px'}}><FontAwesomeIcon icon={faAngleRight} color={'grey'} size={'2x'}/></ButtonNext>
+                    </div>
+                </CarouselProvider>
             </>
         )
     }
