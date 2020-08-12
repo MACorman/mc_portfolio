408d5a5683c371b2509a85bd8aa5e664a44a3450
MACorman <madelinecorman@gmail.com>
2020-08-12 15:06:51 -0400

    Updates

---
+++ precache-manifest.4693347a6b0bd9f7774f54f2a200b8fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d9fad01f4420e891bdcac3140b8cdf",
+    "url": "/mc_portfolio/index.html"
+  },
+  {
+    "revision": "faf322afe731beffce52",
+    "url": "/mc_portfolio/static/css/2.5abc91f4.chunk.css"
+  },
+  {
+    "revision": "cd8a8a67668998a52c17",
+    "url": "/mc_portfolio/static/css/main.395e5fe4.chunk.css"
+  },
+  {
+    "revision": "faf322afe731beffce52",
+    "url": "/mc_portfolio/static/js/2.0b18ff65.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/mc_portfolio/static/js/2.0b18ff65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8a8a67668998a52c17",
+    "url": "/mc_portfolio/static/js/main.7ab0a408.chunk.js"
+  },
+  {
+    "revision": "999a26a2f35276f53c84",
+    "url": "/mc_portfolio/static/js/runtime-main.a7292c22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mc_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
